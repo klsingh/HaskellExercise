dbf8ae30743e8502de5e3418fe9664a621ae42a7
Kumar Lokesh Singh <kumarlokeshsingh@outlook.com>
2022-10-26 13:56:18 +0530

    Second

--- SimpleFunctions.hs
+++ SimpleFunctions.hs
@@ -1,7 +1,15 @@
 module SimpleFunctions where
 
+import System.Win32 (COORD (x))
+
 -- Lambda Expressions
 f :: Integer -> Integer
 f x = 3 * x ^ 2 + 5
 
 -- Example 2 for Simple Functions
+square :: Int -> Int
+square x = x * x
+
+-- Example 3 for Simple Functions
+mySum :: Int -> Int -> Int
+mySum a b = a + b
